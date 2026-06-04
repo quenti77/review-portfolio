c7dfa924def4e6482ebf7bd46adf6be432b607d9
quenti77 <quenti77@proton.me>
2026-06-04 21:11:14 +0200

    chore(review): remove or annotate parts of code
    - Remove unused import in App.jsx
    - Add default fonts
    - Annotate unused store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+// Faire attention avec les tests de notre côté.
+// Tu peux en faire, mais il faut éviter que du code mort se
+// retrouve dans le code final du repository.
 const slice = createSlice({
   name: "slice",
   initialState: "initialState",
